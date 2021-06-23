aa0bbda76cc899d3cc8ded708955dca4b6d7113c
victordoyle <victor-doyle@outlook.com>
2021-06-23 17:56:20 -0400

    base commit with MStoryboard skeleton and img assets

--- QuizzGame/ViewController.swift
+++ QuizzGame/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    // base tests and setup before actual question load-in
+    var questionNumber = 0;
+    let quiz = [
+        "Four + 2 is Equal To 6",
+        "Four + 4 is Equal To 8",
+        "Four + 6 is Equal To 10",
+        "Four + 8 is Equal To 12"
+    
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUIQuestion()
     }
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        questionNumber += 1;
+    }
+    
+    func updateUIQuestion() {
+        questionLabel.text = quiz[questionNumber];
+    }
 
 }
 
